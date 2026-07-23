7ca26d484b6380fedbe9efa127e427a0a2e5d08d
Caleb Ellefson <calebroger@gmail.com>
2026-07-22 22:03:55 -0500

    HAB: lora.c
    Broke Lora logic into its own files for easier testing.
    
    Signed-off-by: Caleb Ellefson <calebroger@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,46 +1,28 @@
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/lora.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(lora_client);
+#ifdef CONFIG_LORA
+#include "lora.h"
+#endif
+
+LOG_MODULE_REGISTER(main);
 
 int main(void) {
-	const struct device *const lora_dev = DEVICE_DT_GET(DT_ALIAS(lora0));
-	struct lora_modem_config config;
-	int ret;
 
-	if (!device_is_ready(lora_dev)) {
-		LOG_ERR("LoRa device not ready");
-		return 0;
-	}
+#ifdef CONFIG_LORA
 
-	/* Regional configuration (915MHz for North America) */
-	config.frequency = 915000000; 
-	config.bandwidth = BW_125_KHZ;     
-	config.datarate = SF_7;           
-	config.coding_rate = CR_4_5; 
-	config.preamble_len = 8;
-	config.tx_power = 14;
-	config.tx = true;
+    if (lora_client_init() < 0) {
+        LOG_ERR("Failed to initialize LoRa client subsystem");
+        return 0;
+    }
 
-	ret = lora_config(lora_dev, &config);
-	if (ret < 0) {
-		LOG_ERR("LoRa config failed");
-		return 0;
-	}
+    while (1) {
+        static const char msg[] = "Hello World";
+        
+        lora_client_send(msg, sizeof(msg));
+        k_sleep(K_SECONDS(5));
+    }
+#endif
 
-	LOG_INF("LoRa Client Started");
-
-	while (1) {
-		static char msg[] = "Hello World";
-		ret = lora_send(lora_dev, msg, sizeof(msg));
-		if (ret < 0) {
-			LOG_ERR("LoRa send failed");
-		} else {
-			LOG_INF("Message sent!");
-		}
-		k_sleep(K_SECONDS(5));
-	}
-	return 0;
+    return 0;
 }
